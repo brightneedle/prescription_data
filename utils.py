a9848acd77f02099089e79501a296992d74f11ad
Noah Clarke Hall <noah.clarke-hall.22@ucl.ac.uk>
2023-03-20 16:57:38 +0000

    refine code, remove redundancies

--- utils.py
+++ utils.py
@@ -10,3 +10,13 @@ antibiotics = {'penicillins'        : '050101',
                 'tetracyclines'     : '050103',
                 'cephalosporins'    : '050102',
                 'aminoglycosides'   : '050104'}
+
+def assign_types(data):
+    data['type'] = -1
+    for type, code in antibiotics.items():
+        mask = data['BNFCode'].str.startswith(code)
+        data['type'].mask(mask, type, inplace=True)
+
+    data = data.loc[data['type']!=-1]
+
+    return data
